be26064fa9b4b00ec9980dddb8cc4d59c3b73847
zhangweisong <zhangweisong.zws@alibaba-inc.com>
2025-12-27 02:47:57 +0800

    fix: update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -81,6 +81,7 @@ pub fn run() {
         )
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_cli::init())
         .setup(|app| {
